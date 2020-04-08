30e4234961e2f7446d6bbf5318dba31e1b32766e
Chelsea Arceo <arceo.chelsea@gmail.com>
2020-04-07 21:26:59 -0400

    Added another array

---
+++ fruitsales02.js
@@ -0,0 +1,7 @@
+let fruitInventory = ["Apples", "Peaches", "Pumpkins", "Endives"];
+let fruitPrices = [1, 1.5, 7, 12];
+let fruitSold = [20, 12, 7, 4];
+
+for (let i = 0; i < fruitInventory.length; i++) {
+    console.log(`${fruitInventory[i]}`);
+}
